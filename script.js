3bce865ee03050a45b18026d4b483d4004c4c203
BirdOne Admin <127312723+birdoneadmin@users.noreply.github.com>
2023-09-22 20:40:57 +0700

    Save

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Install Bootstrap JS */
 var script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js';
 document.head.appendChild(script);

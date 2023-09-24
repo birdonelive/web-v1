d68d4c09c5f7d7b4d81537437369c65c077143df
BirdOne Admin <127312723+birdoneadmin@users.noreply.github.com>
2023-09-24 23:43:30 +0700

    Save

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ var script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js';
 document.head.appendChild(script);
 document.body.appendChild(script);
+
